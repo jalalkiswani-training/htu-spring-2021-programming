9cec07dd242a7def9b8f43e0b22f331afbbbcead
hzrasheed <alrasheedhana@gmail.com>
2021-04-06 13:04:45 +0300

    lecture 6th of April

---
+++ examples/maven/src/main/java/edu/htu/week5/lecture2/IfPractices.java
@@ -0,0 +1,30 @@
+package edu.htu.week5.lecture2;
+
+public class IfPractices {
+
+	public static void main(String[] args) {
+	
+		boolean flag=true;
+		int x=5;
+		int y=10;
+		
+		//
+		// >
+		 // >=
+		// <
+		// < =
+		// != 
+		// == 
+		if(x>y) {
+			System.out.println(x);
+		}
+		else {
+			System.out.println(y);
+		}
+		
+		
+		
+
+	}
+
+}
