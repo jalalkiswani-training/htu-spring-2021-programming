6256f71825bf3d966930841a9eb5286f7d361db8
Jalal Kiswani <kiswanij@gmail.com>
2021-05-09 12:18:57 +0300

    OOP2

---
+++ examples/maven/src/main/java/edu/htu/weekz10/lecture1/Person.java
@@ -0,0 +1,12 @@
+package edu.htu.weekz10.lecture1;
+
+public class Person {
+	// instance variables (attributes)
+	String name;
+	int age;
+
+	// methods (actions)
+	public void sayHello() {
+		System.out.println("Hello, my name is :" + name + ", and my age is : " + age);
+	}
+}
---
+++ examples/maven/src/main/java/edu/htu/weekz10/lecture1/Test.java
@@ -0,0 +1,23 @@
+package edu.htu.weekz10.lecture1;
+
+public class Test {
+	public static void main(String[] args) {
+		//Person: datatype - class name
+		//p: variable (in the case of class -> reference)
+		//new Person(): construction
+		Person p=new Person();
+		p.name="Ata";
+		p.age=50;
+		
+		p.sayHello();
+		
+		
+		Student s=new Student();
+		s.name="Lucy";
+		s.age=20;
+		s.gpa=99;
+		s.sayHello();
+		s.sayHelloWithGpa();
+		
+	}
+}
